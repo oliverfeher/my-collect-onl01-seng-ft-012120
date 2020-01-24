1dc20d050472fd3ad7c019f05a59fc4648510130
Oliver Feher <olcsee@gmail.com>
2020-01-24 04:23:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   array2 = []
   while i < array.length
-    array2.push(yield(array[i].split(" ").first))
+    array2.push(yield(array[i]))
     i += 1
   end
   array2
